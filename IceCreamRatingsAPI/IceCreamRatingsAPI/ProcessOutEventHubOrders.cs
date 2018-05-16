b5052baad48f9ce347c6b85641cf69d4f8007869
Amar Gowda <amgowda@microsoft.com>
2018-05-16 08:01:58 -0700

    added event hub function

--- IceCreamRatingsAPI/IceCreamRatingsAPI/ProcessOutEventHubOrders.cs
+++ IceCreamRatingsAPI/IceCreamRatingsAPI/ProcessOutEventHubOrders.cs
@@ -1,15 +1,28 @@
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Azure.WebJobs.ServiceBus;
+using Newtonsoft.Json;
+using System.Threading.Tasks;
 
 namespace IceCreamRatingsAPI
 {
     public static class ProcessOutEventHubOrders
     {
         [FunctionName("ProcessOutEventHubOrders")]
-        public static void Run([EventHubTrigger("challenge7post1", Connection = "EVentHubConn")]string myEventHubMessage, TraceWriter log)
+        public static async Task Run([EventHubTrigger("eventhubpos", Connection = "EVentHubConn")]string[] myEventHubMessage, TraceWriter log)
         {
             log.Info($"C# Event Hub trigger function processed a message: {myEventHubMessage}");
+            POS newpositem = new POS();
+
+           foreach(string item in myEventHubMessage)
+            {
+                newpositem = JsonConvert.DeserializeObject<POS>(item);
+
+              bool success= await BatchSavePOS.SavePOSAsync(newpositem);
+
+                log.Info($"{item}\r\n");
+
+            }
         }
     }
 }
