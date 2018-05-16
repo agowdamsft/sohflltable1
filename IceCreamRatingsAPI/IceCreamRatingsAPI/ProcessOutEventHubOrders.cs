6a8546ca3a6071689657d725078a6fed09f9688a
Amar Gowda <amgowda@microsoft.com>
2018-05-16 08:13:44 -0700

    ch#7 complete

--- IceCreamRatingsAPI/IceCreamRatingsAPI/ProcessOutEventHubOrders.cs
+++ IceCreamRatingsAPI/IceCreamRatingsAPI/ProcessOutEventHubOrders.cs
@@ -11,7 +11,7 @@ namespace IceCreamRatingsAPI
         [FunctionName("ProcessOutEventHubOrders")]
         public static async Task Run([EventHubTrigger("eventhubpos", Connection = "EVentHubConn")]string[] myEventHubMessage, TraceWriter log)
         {
-            log.Info($"C# Event Hub trigger function processed a message: {myEventHubMessage}");
+            log.Info($"C# Event Hub trigger function processed a message Length: {myEventHubMessage.Length}");
             POS newpositem = new POS();
 
            foreach(string item in myEventHubMessage)
@@ -19,8 +19,11 @@ namespace IceCreamRatingsAPI
                 newpositem = JsonConvert.DeserializeObject<POS>(item);
 
               bool success= await BatchSavePOS.SavePOSAsync(newpositem);
+                if(success)
+                log.Info($"{item} \r\n");
 
-                log.Info($"{item}\r\n");
+                else
+                    log.Info($"Failed to process 1 item");
 
             }
         }
