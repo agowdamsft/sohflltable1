1a6eb69e200067cf75a15584072bd7aaf9fe8bc2
Fabrício Sanchez <fabsanc@microsoft.com>
2018-05-16 13:14:05 -0400

    Adding sentiment analysis to CreateRating function

---
+++ IceCreamRatingsAPI/IceCreamRatingsAPI/Sentiment.cs
@@ -0,0 +1,43 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IceCreamRatingsAPI
+{
+    public partial class Sentiment
+    {
+        [JsonProperty("documents")]
+        public DocumentSentiment[] Documents { get; set; }
+    }
+
+    public partial class DocumentSentiment
+    {
+        [JsonProperty("language")]
+        public string Language { get; set; }
+
+        [JsonProperty("id")]
+        public string Id { get; set; }
+
+        [JsonProperty("text")]
+        public string Text { get; set; }
+    }
+
+    public partial class SentimentReturn
+    {
+        [JsonProperty("documents")]
+        public DocumentReturn[] Documents { get; set; }
+
+        [JsonProperty("errors")]
+        public object[] Errors { get; set; }
+    }
+
+    public partial class DocumentReturn
+    {
+        [JsonProperty("score")]
+        public double Score { get; set; }
+
+        [JsonProperty("id")]
+        public string Id { get; set; }
+    }
+}
